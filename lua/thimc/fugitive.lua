76b6d9ee1fbed6473f6797ee1768f9cfc19d7ea9
Thim Cederluind <thim@cederlund.de>
2023-03-28 18:41:50 +0200

    <leader>gL shows commit history in quickfixes (0Gclog)

--- lua/thimc/fugitive.lua
+++ lua/thimc/fugitive.lua
@@ -1,5 +1,6 @@
 local bind = vim.keymap.set
 bind('n', '<leader>gd', "<cmd>Git diff<CR>")
 bind('n', '<leader>gl', "<cmd>Git log<CR>")
+bind('n', '<leader>gL', "<cmd>0Gclog<CR>")
 bind('n', '<leader>gs', "<cmd>Git status<CR>")
 bind('n', '<leader>gg', ":Git ")
