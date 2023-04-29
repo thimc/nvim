c1b748b27a58d8d6e9a5c0fe8c01977353d5ac86
Thim Cederluind <thim@cederlund.de>
2023-04-29 20:43:42 +0200

    fix

--- lua/thimc/lsp.lua
+++ lua/thimc/lsp.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
 	callback = function(ev)
 		local opts = {buffer = bufnr}
+		local bind = vim.keymap.set
 
 		-- Enable completion triggered by <c-x><c-o>
 		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
